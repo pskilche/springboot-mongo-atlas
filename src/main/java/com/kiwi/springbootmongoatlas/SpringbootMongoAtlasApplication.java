cbc3d1a4debbf1675ea22c05654d40ac26f1cf2e
pkilche <pkilche@ebates.com>
2024-03-26 19:42:01 -0400

    Initial commit

---
+++ src/main/java/com/kiwi/springbootmongoatlas/SpringbootMongoAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.springbootmongoatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoAtlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoAtlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiwi/springbootmongoatlas/SpringbootMongoAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.springbootmongoatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
